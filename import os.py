4020c460702ecd5bd1331e6135c523d9891c9ff3
Zeyad Mohammed slama <93527235+Demo-23home@users.noreply.github.com>
2022-08-26 18:10:40 +0200

    HTMLtask

---
+++ import os.py
@@ -0,0 +1,8 @@
+import os
+from posixpath import dirname
+path="D:\My Photos\old photos"
+for dirpath , dirname,filename, in os.walk(path):
+    print(f'Path :{dirpath}')
+    print(f'Dir: {dirname}')
+    print(f'File: {filename}')
+    print('-------------------')
